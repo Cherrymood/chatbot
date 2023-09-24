6509fa37f24e5e5b9e053e8057b652056718c110
Valeriia <121061578+Cherrymood@users.noreply.github.com>
2023-09-23 20:33:34 -0700

    Entered a loop to take user input and display response

--- Program.cs
+++ Program.cs
@@ -6,5 +6,22 @@
                 var chatGPTClient = new ChatGPTClient(apiKey);
 
                 Console.WriteLine("Welcome to the ChatGPT chatbot! Type 'exit' to quit.");
+            while (true)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.Write("You: ");
+                    Console.ResetColor();
+                    string input = Console.ReadLine() ?? string.Empty;
+
+                    if (input.ToLower() == "exit")
+                        break;
+
+                    string response = chatGPTClient.SendMessage(input);
+
+                    Console.ForegroundColor = ConsoleColor.Yellow; 
+                    Console.Write("Chatbot: ");
+                    Console.ResetColor(); 
+                    Console.WriteLine(response);
+                }
             }
         }
