531c7bc556ceeab3f689841cc5320067bd5967f0
Valeriia <121061578+Cherrymood@users.noreply.github.com>
2023-09-23 20:26:53 -0700

    Added the JSON body to the request

--- SendMessage.cs
+++ SendMessage.cs
@@ -1,14 +1,16 @@
 public string SendMessage(string message)
-        {
-            var request = new RestRequest("", Method.Post);
-            request.AddHeader("Content-Type", "application/json");
-            request.AddHeader("Authorization", $"Bearer {_apiKey}");
+{
+    var request = new RestRequest("", Method.Post);
+    request.AddHeader("Content-Type", "application/json");
+    request.AddHeader("Authorization", $"Bearer {_apiKey}");
 
-            var requestBody = new
-            {
-                prompt = message,
-                max_tokens = 100,
-                n = 1,
-                stop = (string?)null,
-                temperature = 0.7,
-            };
+    var requestBody = new
+        {
+            prompt = message,
+            max_tokens = 100,
+            n = 1,
+            stop = (string?)null,
+            temperature = 0.7,
+        };
+    request.AddJsonBody(JsonConvert.SerializeObject(requestBody));
+}
