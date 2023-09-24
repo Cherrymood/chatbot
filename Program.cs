239614bbe374c7dce8116ba7c2b55dec6680e728
Valeriia <121061578+Cherrymood@users.noreply.github.com>
2023-09-23 20:30:19 -0700

    Replaced with your ChatGPT API key

--- Program.cs
+++ Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿class Program
+        {
+            static void Main(string[] args)
+            {
+                string apiKey = "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";
+
+            }
+        }
