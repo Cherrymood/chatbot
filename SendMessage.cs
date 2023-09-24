b9c526ed569db555833637901ff1c3684e592f9e
Valeriia <121061578+Cherrymood@users.noreply.github.com>
2023-09-23 20:24:28 -0700

    Created the request body with the message and other parameters

--- SendMessage.cs
+++ SendMessage.cs
@@ -3,4 +3,12 @@ public string SendMessage(string message)
             var request = new RestRequest("", Method.Post);
             request.AddHeader("Content-Type", "application/json");
             request.AddHeader("Authorization", $"Bearer {_apiKey}");
-            
+
+            var requestBody = new
+            {
+                prompt = message,
+                max_tokens = 100,
+                n = 1,
+                stop = (string?)null,
+                temperature = 0.7,
+            };
