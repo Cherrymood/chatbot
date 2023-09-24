c93a3e98775e15c9abe3ec5625c5a3ee8d9e3ee7
Valeriia <121061578+Cherrymood@users.noreply.github.com>
2023-09-23 20:31:38 -0700

    Displayed a welcome message

--- Program.cs
+++ Program.cs
@@ -4,5 +4,7 @@
             {
                 string apiKey = "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";
                 var chatGPTClient = new ChatGPTClient(apiKey);
+
+                Console.WriteLine("Welcome to the ChatGPT chatbot! Type 'exit' to quit.");
             }
         }
