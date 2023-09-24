1da4336cba762ad22098a63493a150b45916f6eb
Valeriia <121061578+Cherrymood@users.noreply.github.com>
2023-09-23 20:23:40 -0700

    Set the Authorization header with the API key

--- SendMessage.cs
+++ SendMessage.cs
@@ -1,3 +1,6 @@
 public string SendMessage(string message)
         {
             var request = new RestRequest("", Method.Post);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("Authorization", $"Bearer {_apiKey}");
+            
