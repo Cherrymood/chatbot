6665c935f58d431d1998cbb18491b67c6edf641e
Valeriia <121061578+Cherrymood@users.noreply.github.com>
2023-09-23 21:14:00 -0700

    Modifed

--- ChatGPTClient.cs
+++ ChatGPTClient.cs
@@ -1,6 +1,6 @@
 using System;
-using RrestSharp;
-using Json;
+using RestSharp;
+using json;
 
 
 public class ChatGPTClient
@@ -12,6 +12,26 @@ public class ChatGPTClient
     {
         _apiKey = apiKey;
         _client = new RestClient ("https://api.openai.com/v1/engines/text-davinci-003/completions");
+    }
+
+    public string SendMessage(string message)
+    {
+        var request = new RestRequest("", Method.Post);
+        request.AddHeader("Content-Type", "application/json");
+        request.AddHeader("Authorization", $"Bearer {_apiKey}");
+
+        var requestBody = new
+            {
+                prompt = message,
+                max_tokens = 100,
+                n = 1,
+                stop = (string?)null,
+                temperature = 0.7,
+            };
+        request.AddJsonBody(JsonConvert.SerializeObject(requestBody));
+        var response = _client.Execute(request);
+        var jsonResponse = JsonConvert.DeserializeObject<dynamic>(response.Content ?? string.Empty);
+        return jsonResponse?.choices[0]?.text?.ToString()?.Trim() ?? string.Empty;
     }
 
 }
