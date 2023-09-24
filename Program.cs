326055e693a985d2f696d667e1e102080567ea88
Valeriia <121061578+Cherrymood@users.noreply.github.com>
2023-09-23 20:30:55 -0700

    Created a ChatGPTClient instance with the API key

--- Program.cs
+++ Program.cs
@@ -3,6 +3,6 @@
             static void Main(string[] args)
             {
                 string apiKey = "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";
-
+                var chatGPTClient = new ChatGPTClient(apiKey);
             }
         }
